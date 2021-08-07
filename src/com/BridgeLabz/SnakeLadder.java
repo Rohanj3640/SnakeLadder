0823d0f4f6bcb941330746c0fc321bf34c53bc3a
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-07 10:28:18 -0700

    Added: Snake Ladder Welcome Message

---
+++ src/com/BridgeLabz/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+        int startPosition = 0;                                        // initialize startPosition to zero
+        System.out.println("Single Player Game Started At Position 0");
+
+    }
+}
